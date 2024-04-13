78e803f2dde2893924781316522c7b71e4254c0e
Quails <centimofu@gmail.com>
2024-04-13 18:54:38 +0100

    :building_construction: Add navigation to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { Route } from "@/interfaces/router";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +10,17 @@ export const metadata: Metadata = {
   description: "A Pokémon TCG fansite",
 };
 
+const routes: Route[] = [
+  {
+    label: "Home",
+    uri: "/",
+  },
+  {
+    label: "About",
+    uri: "/about",
+  },
+];
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,7 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          {routes.map((route) => (
+            <Link href={route.uri}>{route.label}</Link>
+          ))}
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
