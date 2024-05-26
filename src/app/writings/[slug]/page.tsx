8314bc316b6bfbb216b326ad8f0c8a92af9b8653
Quails <centimofu@gmail.com>
2024-05-26 16:37:17 +0100

    :goal_net: Handle mistyped writing slug
    Wraps the writing metadata and page render functions in a try-catch block. Renders a 404 page if no writing is found

--- src/app/writings/[slug]/page.tsx
+++ src/app/writings/[slug]/page.tsx
@@ -16,32 +16,51 @@ export const generateMetadata = async ({
 }: {
   params: { slug: string };
 }) => {
-  const writingMetadata = getWritingMetadataBySlug(params.slug);
-  return {
-    title: getMetadataTitle(writingMetadata.title),
-  };
+  try {
+    const writingMetadata = getWritingMetadataBySlug(params.slug);
+    return {
+      title: getMetadataTitle(writingMetadata.title),
+    };
+  } catch (error) {
+    return {
+      title: getMetadataTitle("Writing not found"),
+    };
+  }
 };
 
 const Writing = ({ params }: { params: { slug: string } }) => {
   const slug = params.slug;
-  const writingContent = getWritingContentBySlug(slug);
 
-  return (
-    <main>
-      <Markdown
-        options={{
-          wrapper: "article",
-          overrides: {
-            h1: { component: "h2" },
-            h2: { component: "h3" },
-            h3: { component: "h4" },
-          },
-        }}
-      >
-        {writingContent}
-      </Markdown>
-    </main>
-  );
+  try {
+    const writingContent = getWritingContentBySlug(slug);
+
+    return (
+      <main>
+        <Markdown
+          options={{
+            wrapper: "article",
+            overrides: {
+              h1: { component: "h2" },
+              h2: { component: "h3" },
+              h3: { component: "h4" },
+            },
+          }}
+        >
+          {writingContent}
+        </Markdown>
+      </main>
+    );
+  } catch (error) {
+    return (
+      <main>
+        <h2>Writing not found</h2>
+        <p>
+          Could not find the corresponding writing. Please check the URL or
+          return to the writings index and try again.
+        </p>
+      </main>
+    );
+  }
 };
 
 export default Writing;
